925c0c8c8780fa136b995aa7ab8ae12512c4b181
Alexandre Saldanha <absaldanha@protonmail.com>
2025-06-28 02:12:55 -0300

    Change structure

--- template.rb
+++ template.rb
@@ -17,7 +17,7 @@ def add_template_repository_to_source_path
       tempdir
     ].map(&:shellescape).join(" ")
 
-    if (branch = __FILE__[%r{jumpstart/(.+)/template.rb}, 1])
+    if (branch = __FILE__[%r{rails-template/(.+)/template.rb}, 1])
       Dir.chdir(tempdir) { git checkout: branch }
     end
   else
