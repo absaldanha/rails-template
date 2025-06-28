35fa82bfe42ba6b258ddcd1e64be1efcfeec7f6e
Alexandre Saldanha <absaldanha@protonmail.com>
2025-06-28 01:50:29 -0300

    Update constrained_form_generator.rb

--- lib/generators/constrained_form/constrained_form_generator.rb
+++ lib/generators/constrained_form/constrained_form_generator.rb
@@ -1,11 +1,1 @@
-# class ConstrainedFormGenerator < Rails::Generators::Base
-#   source_root File.expand_path("templates", __dir__)
-
-#   def create_form_builder
-#     template "lib/constrained_form/form_builder.rb"
-#   end
-# end
-
-# source_root File.expand_path("templates", __dir__)
-
 copy_file "lib/constrained_form/form_builer.rb"
